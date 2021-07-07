c99394242f1f34a89a7bf9d33ffe4f731d285f47
Ayush <ayushtripathi9197@gmail.com>
2021-07-07 14:54:39 +0530

    image functionality

--- src/components/memories/CreateMemoryForm.vue
+++ src/components/memories/CreateMemoryForm.vue
@@ -6,8 +6,15 @@
         <ion-input type="text" required v-model="enteredTitle" />
       </ion-item>
       <ion-item>
-        <ion-label position="floating">Image URL</ion-label>
-        <ion-input type="url" required v-model="enteredImageUrl" />
+        <ion-thumbnail slot="start">
+          <img :src="previewImageUrl" alt="" />
+        </ion-thumbnail>
+        <ion-button type="button" fill="clear" @click="takePhoto">
+          <ion-icon slot="start" :icon="camera"></ion-icon>
+          Take Photo
+        </ion-button>
+        <!-- <ion-label position="floating">Image URL</ion-label>
+        <ion-input type="url" required v-model="enteredImageUrl" /> -->
       </ion-item>
       <ion-item>
         <ion-label position="floating">Description</ion-label>
@@ -27,29 +34,53 @@ import {
   IonItem,
   IonTextarea,
   IonButton,
+  IonThumbnail,
+  IonIcon,
 } from "@ionic/vue";
+import { camera } from "ionicons/icons";
+import { Camera, CameraResultType, CameraSource } from "@capacitor/camera";
 export default {
   emits: ["save-memory"],
   components: {
+    IonThumbnail,
     IonLabel,
     IonList,
     IonInput,
     IonItem,
     IonTextarea,
     IonButton,
+    IonIcon,
   },
   data() {
     return {
       enteredTitle: "",
-      enteredImageUrl: "",
       enteredDescription: "",
+      previewImageUrl: null,
+      camera,
     };
   },
   methods: {
+    async takePhoto() {
+      const photo = await Camera.getPhoto({
+        quality: 90,
+        source: CameraSource.Camera,
+        allowEditing: true,
+        resultType: CameraResultType.Uri,
+      });
+
+      // image.webPath will contain a path that can be set as an image src.
+      // You can access the original file using image.path, which can be
+      // passed to the Filesystem API to read the raw data of the image,
+      // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+      this.previewImageUrl = photo.webPath;
+
+      // Can be set to the src of an image now
+      // imageElement.src = imageUrl;
+    },
     submitForm() {
       const memoryData = {
         title: this.enteredTitle,
-        imageUrl: this.enteredImageUrl,
+        imageUrl: this.previewImageUrl,
         description: this.enteredDescription,
       };
       this.$emit("save-memory", memoryData);
